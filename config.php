11c0993cf3df30fc126a7dd694f3bf0ac8c3bf73
jamwil907 <jamwil907@gmail.com>
2025-11-18 19:53:32 -0500

    Update config.php
    header config file

--- config.php
+++ config.php
@@ -1,3 +1,4 @@
+##Configuration PHP file to include API key
 <?php
 // OutCast Weather App Configuration
 // Get your free API key from https://openweathermap.org/api
@@ -6,4 +7,5 @@ define('OPENWEATHER_API_KEY', 'c7ceef4f38da123569a1e403ac41c638');
 
 define('OPENWEATHER_API_URL', 'https://api.openweathermap.org/data/2.5/forecast');
 // For current weather: https://api.openweathermap.org/data/2.5/weather
+
 
